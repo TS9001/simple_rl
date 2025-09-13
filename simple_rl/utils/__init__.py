0bb1e918219c0c03e4f0a6c7e820baf70f9b4b8c
Tomas Sikora <tomas.sikora@gnosis.io>
2025-09-13 12:36:26 +0200

    More clean-ups

--- simple_rl/utils/__init__.py
+++ simple_rl/utils/__init__.py
@@ -3,11 +3,9 @@ Utilities module.
 """
 
 from .logging import setup_logging
-from .data import DatasetLoader
 from .huggingface_wrappers import LanguageModel
 
 __all__ = [
     "setup_logging", 
-    "DatasetLoader",
     "LanguageModel",
 ]
